101b55d522983b5a5a10ca13aa98c7bbab12ee14
Roman Tselishhev <roman.ts@mail.ru>
2021-04-18 20:15:49 +0500

    new text for netology

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Roman!");
-        System.out.println(" ");
+        System.out.println("прохожу обучение в НЕТОЛОГИИ");
 
     }
 
